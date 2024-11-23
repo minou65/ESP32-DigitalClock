3bbe898bd010b55203d822c55982451e94dc3343
Andy <azogg65@bluewin.ch>
2024-11-23 07:28:22 +0100

    first commit

---
+++ ESP32-Wanduhr.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP32_Wanduhr.ino
+ Created:	23.11.2024 07:13:52
+ Author:	andy
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
