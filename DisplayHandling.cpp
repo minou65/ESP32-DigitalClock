882100749dffbc3b9aebe24513776f6448921d75
Andy <azogg65@bluewin.ch>
2025-03-14 07:15:49 +0100

    Increase RGB bounce buffer size for improved display performance

--- DisplayHandling.cpp
+++ DisplayHandling.cpp
@@ -574,7 +574,7 @@ void initDisplay() {
     // When avoid tearing function is enabled, configure the bus according to the LVGL configuration
     ESP_PanelBus* lcd_bus_ = panel->getLcd()->getBus();
 #if ESP_PANEL_LCD_BUS_TYPE == ESP_PANEL_BUS_TYPE_RGB
-    static_cast<ESP_PanelBus_RGB*>(lcd_bus_)->configRgbBounceBufferSize(ESP_PANEL_LCD_WIDTH * 20);
+    static_cast<ESP_PanelBus_RGB*>(lcd_bus_)->configRgbBounceBufferSize(ESP_PANEL_LCD_WIDTH * 40);
     static_cast<ESP_PanelBus_RGB*>(lcd_bus_)->configRgbFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
 #elif ESP_PANEL_LCD_BUS_TYPE == ESP_PANEL_BUS_TYPE_MIPI_DSI
     static_cast<ESP_PanelBus_DSI*>(lcd_bus_)->configDpiFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
